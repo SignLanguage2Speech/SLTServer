10c545aeb4d02b6c8e102d1337658c6ccd0c8a81
anden1305 <andreasbigom@gmail.com>
2023-04-25 16:23:54 +0200

    Adding whisper stt server

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from model.model import TestModel
 from server.server import ModelServer
+from model.speech_to_text import SpeechToText
 
 if __name__ == '__main__':
     model = TestModel()
-    model_server = ModelServer(model)
+    tts_model = SpeechToText()
+    model_server = ModelServer(model,tts_model)
     model_server.run()
--- server/server.py
+++ server/server.py
@@ -3,14 +3,19 @@ from model.test import test_write_streamed_video, test_load_webm_from_bytes
 
 from flask import Flask
 from flask_sock import Sock
+import pdb
 
 class ModelServer:
-    def __init__(self, model):
+    def __init__(self, model, tts_model):
         self.app = Flask(__name__)
         self.sock = Sock(self.app)
         self.model = model
+        self.tts_model = tts_model
         self.initialize_routes()
         self.initialize_sock()
+        self.spoken_language = "english"
+        self.signed_language_from = "english"
+        self.signed_language_to = "english"
     
     def initialize_routes(self):
         @self.app.route('/1')
@@ -34,6 +39,12 @@ class ModelServer:
                 # test_write_streamed_video(tns_data)
                 test_load_webm_from_bytes(data)
                 ws.send(data)
+        @self.sock.route("/stt")
+        def stt(ws):
+            while True:
+                data = ws.receive()
+                out = self.tts_model.inference(data,self.spoken_language)
+                ws.send(out)
 
     def run(self):
         self.app.run(
